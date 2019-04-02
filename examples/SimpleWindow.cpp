d0b89038251dde38ba6c4d1c91ba1bd8d6d76213
beProsto <48763903+beProsto@users.noreply.github.com>
2019-04-02 20:49:41 +0200

    Simple Window creating app.
    Really easy application that displays a black window.

---
+++ examples/SimpleWindow.cpp
@@ -0,0 +1,42 @@
+#include <venum/window.hpp>
+#include <venum/fpslimiter.hpp>
+#include <venum/event.hpp>
+#include <venum/renderer.hpp>
+
+/* Main function */
+int main( int argc, char** argv )
+{
+		/* Creating Window named "window" with a title "Title of a Window" */
+    ven::Window window( "Title of a Window" );
+		/* Creating FPSLimiter named "fps" with limit of 60 fps (15 is the invertion) */
+    ven::FPSLimiter fps( 15 );
+		
+		/* Creating Event named "event" */
+    ven::Event event;
+		/* Attaching "window" as a window pointer to "event"s Mouse */
+    event.Mouse.AttachWindowPointer( &window );
+		
+		/* Creating Renderer named "renderer" */
+		ven::Renderer renderer;
+		
+		/* Starting fps measurements */
+		fps.Start(  );
+		
+		/* Loop running while the window is running */
+		while( window.Running(  ) )
+		{
+				/* Polling an Event from "window". Returns true, if event happened. */
+        window.PollEvent( &event.Event );
+				
+				/* Cleaning renderers buffer */
+				renderer.Clean(  );
+				
+				/* Swapping windows buffer */
+				window.Swapb(  );
+				/* Updating fps */
+        fps.Update(  );
+		}
+		
+		/* Returning 0 when program closes */
+		return 0;
+}
