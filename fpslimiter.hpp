274f33eb778d6eda0315adf1689b4b7e02af64b8
beProsto <48763903+beProsto@users.noreply.github.com>
2019-04-02 21:12:39 +0200

    Added comments.

--- fpslimiter.hpp
+++ fpslimiter.hpp
@@ -1,8 +1,10 @@
 #ifndef FPSLIMITER_HPP_INCLUDED
 #define FPSLIMITER_HPP_INCLUDED
 
+/* including needed library (SDL2) */
 #include <SDL2/SDL.h>
 
+/* including class and code files */
 #include "class/fpslimiterclass.hpp"
 #include "code/fpslimitercode.hpp"
 
