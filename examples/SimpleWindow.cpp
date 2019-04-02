8712310bcd6bb31622778c40d5cef6885b437d04
beProsto <48763903+beProsto@users.noreply.github.com>
2019-04-02 20:51:33 +0200

    Update SimpleWindow.cpp

--- examples/SimpleWindow.cpp
+++ examples/SimpleWindow.cpp
@@ -6,37 +6,37 @@
 /* Main function */
 int main( int argc, char** argv )
 {
-		/* Creating Window named "window" with a title "Title of a Window" */
-    ven::Window window( "Title of a Window" );
-		/* Creating FPSLimiter named "fps" with limit of 60 fps (15 is the invertion) */
-    ven::FPSLimiter fps( 15 );
+	/* Creating Window named "window" with a title "Title of a Window" */
+	ven::Window window( "Title of a Window" );
+	/* Creating FPSLimiter named "fps" with limit of 60 fps (15 is the invertion) */
+    	ven::FPSLimiter fps( 15 );
 		
-		/* Creating Event named "event" */
-    ven::Event event;
-		/* Attaching "window" as a window pointer to "event"s Mouse */
-    event.Mouse.AttachWindowPointer( &window );
+	/* Creating Event named "event" */
+    	ven::Event event;
+	/* Attaching "window" as a window pointer to "event"s Mouse */
+    	event.Mouse.AttachWindowPointer( &window );
 		
-		/* Creating Renderer named "renderer" */
-		ven::Renderer renderer;
+	/* Creating Renderer named "renderer" */
+	ven::Renderer renderer;
 		
-		/* Starting fps measurements */
-		fps.Start(  );
+	/* Starting fps measurements */
+	fps.Start(  );
 		
-		/* Loop running while the window is running */
-		while( window.Running(  ) )
-		{
-				/* Polling an Event from "window". Returns true, if event happened. */
-        window.PollEvent( &event.Event );
+	/* Loop running while the window is running */
+	while( window.Running(  ) )
+	{
+		/* Polling an Event from "window". Returns true, if event happened. */
+        	window.PollEvent( &event.Event );
 				
-				/* Cleaning renderers buffer */
-				renderer.Clean(  );
+		/* Cleaning renderers buffer */
+		renderer.Clean(  );
 				
-				/* Swapping windows buffer */
-				window.Swapb(  );
-				/* Updating fps */
-        fps.Update(  );
-		}
+		/* Swapping windows buffer */
+		window.Swapb(  );
+		/* Updating fps */
+        	fps.Update(  );
+	}
 		
-		/* Returning 0 when program closes */
-		return 0;
+	/* Returning 0 when program closes */
+	return 0;
 }
