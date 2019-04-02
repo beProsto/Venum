dac0f5f39eead374d054d7d0ca6f7e42169f6fcb
beProsto <48763903+beProsto@users.noreply.github.com>
2019-04-02 21:30:23 +0200

    Added comments

--- gl/glcall.hpp
+++ gl/glcall.hpp
@@ -4,23 +4,30 @@
 #include <iostream>
 #include <GL/glew.h>
 
+/* GLCall definition, calls OpenGL function and checks errors */
 #define GLCall(x) ven::GLClearError(  ); x; assert(ven::GLLogCall(  ));
 
 namespace ven
 {
 
+/* function clearing the errors */
 void GLClearError(  )
 {
     while( glGetError(  ) != GL_NO_ERROR );
 }
 
+/* function checking errors message */
 bool GLLogCall(  )
 {
+    /* when glGetError returns true */
     while( GLenum Error = glGetError(  ) )
     {
+        /* output the error */
         std::cerr << "[GL ERROR]" << "(" << Error << ")" << '\n';
+        /* return false */
         return false;
     }
+    /* return true if the error hasn't been seen */
     return true;
 }
 
