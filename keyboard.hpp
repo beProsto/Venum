8a958532a2e892c2ca4fc5491798fb8ff2d1ae39
beProsto <48763903+beProsto@users.noreply.github.com>
2019-04-02 21:31:48 +0200

    Added comments

--- keyboard.hpp
+++ keyboard.hpp
@@ -1,8 +1,10 @@
 #ifndef KEYBOARD_HPP_INCLUDED
 #define KEYBOARD_HPP_INCLUDED
 
+/* including needed library (SDL2) */
 #include <SDL2/SDL.h>
 
+/* including class and code files */
 #include "class/keyboardclass.hpp"
 #include "code/keyboardcode.hpp"
 
